9b4d54cd9d67716349b5fd42d5bf1c0b3b222a12
velskii <posixie@gmail.com>
2021-11-23 21:31:33 -0500

    test

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var SERVER_NAME = 'user-api'
-var PORT = process.env.PORT;
+var SERVER_NAME = 'test-api'
+var PORT = 80;
 
 
 var restify = require('restify')
-
-  , usersSave = require('save')('users')
-
   , server = restify.createServer({ name: SERVER_NAME})
 
   server.listen(PORT, function () {
@@ -16,10 +13,12 @@ server
   .use(restify.fullResponse())
   .use(restify.bodyParser())
 
-  server.get('/users', function (req, res, next) {
-    usersSave.find({}, function (error, users) {
-      res.send(users)
-    })
+  server.get('/', function (req, res, next) {
+    res.send(JSON.stringify({info:success, code:200}))
+  })
+
+  server.get('/index', function (req, res, next) {
+    res.send(JSON.stringify({info:success, code:200}))
   })
 
 
